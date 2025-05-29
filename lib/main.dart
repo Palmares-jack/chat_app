abc8e793a9ac903c12ad0f9ae52415bac43c453f
Palmares-jack <palmaresjack81@gmail.com>
2025-05-30 01:23:38 +0800

    Creating multiline text fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:chat_app/chat_page.dart';
+import 'package:chat_app/login_page.dart';
+import 'package:chat_app/counter_stateful_demo.dart';
 
 void main() {
   runApp(const ChatApp());
 }
 
 class ChatApp extends StatelessWidget {
-  const ChatApp({super.key});
+  const ChatApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Chat App!!!',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      drawer: const Drawer(),
-      body: const Center(
-        child: Text('Hello!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Button clicked');
-        },
-        child: const Icon(Icons.login),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      drawer: const Drawer(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      // Uncomment one of the following as the app’s home:
+      // home: CounterStateful(buttonColor: Colors.blue),
+      // home: const LoginPage(),
+      home: const ChatPage(),
     );
   }
 }
