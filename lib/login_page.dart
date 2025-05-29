c584dd94fb2684af3db73405d99ff4191c3ea67b
Palmares-jack <palmaresjack81@gmail.com>
2025-05-30 01:19:26 +0800

    Validating user inputs

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,26 +8,27 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController userNameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   void dispose() {
-    userNameController.dispose();
-    passwordController.dispose();
+    _userNameController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
 
-  void loginUser() {
-    debugPrint('Username: ${userNameController.text}');
-    debugPrint('Password: ${passwordController.text}');
-    debugPrint('Login successful!');
-    // TODO: replace prints with real authentication logic
+  void _loginUser() {
+    if (_formKey.currentState?.validate() ?? false) {
+      debugPrint('Username: ${_userNameController.text}');
+      debugPrint('Password: ${_passwordController.text}');
+      debugPrint('Login successful!');
+      // TODO: Navigate or call backend
+    } else {
+      debugPrint('Form is invalid');
+    }
   }
-
-  void _handleLinkTap() => debugPrint('Link clicked!');
-  void _handleDoubleTap() => debugPrint('Double tapped!');
-  void _handleLongPress() => debugPrint('Long pressed!');
 
   @override
   Widget build(BuildContext context) {
@@ -35,86 +36,98 @@ class _LoginPageState extends State<LoginPage> {
       body: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                'Let\'s sign you in!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Welcome back!\nYou\'ve been missed!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                  color: Colors.blueGrey,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Image.network(
-                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                height: 200,
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: userNameController,
-                decoration: const InputDecoration(
-                  hintText: 'Add your username',
-                  hintStyle: TextStyle(color: Colors.blueGrey),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 12),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: 'Type your password',
-                  hintStyle: TextStyle(color: Colors.blueGrey),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: loginUser,
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Let\'s sign you in!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              InkWell(
-                splashColor: Colors.red,
-                onTap: _handleLinkTap,
-                onDoubleTap: _handleDoubleTap,
-                onLongPress: _handleLongPress,
-                child: Column(
-                  children: const [
-                    Text('Find us on'),
-                    SizedBox(height: 4),
-                    Text(
-                      'https://google.com',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                  ],
+                const SizedBox(height: 10),
+                const Text(
+                  'Welcome back!\nYou\'ve been missed!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.blueGrey,
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 20),
+                Image.network(
+                  'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                  height: 200,
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _userNameController,
+                  decoration: const InputDecoration(
+                    hintText: 'Add your username',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please type your username';
+                    }
+                    if (value.length < 5) {
+                      return 'Username must be at least 5 characters';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Type your password',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please type your password';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  onPressed: _loginUser,
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                InkWell(
+                  splashColor: Colors.red,
+                  onTap: () => debugPrint('Link clicked!'),
+                  onDoubleTap: () => debugPrint('Double tapped!'),
+                  onLongPress: () => debugPrint('Long pressed!'),
+                  child: Column(
+                    children: const [
+                      Text('Find us on'),
+                      SizedBox(height: 4),
+                      Text(
+                        'https://google.com',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
