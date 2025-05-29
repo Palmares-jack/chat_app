16eabe47c609a2629deb031970121ca70fa1029a
Palmares-jack <palmaresjack81@gmail.com>
2025-05-30 01:17:21 +0800

    Getting inputs from the user

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    userNameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   void loginUser() {
-    print('Login successful!');
+    debugPrint('Username: ${userNameController.text}');
+    debugPrint('Password: ${passwordController.text}');
+    debugPrint('Login successful!');
+    // TODO: replace prints with real authentication logic
   }
 
-  void handleUrlTap() {
-    print('Pressed on the URL!');
-  }
-
-  void handleDoubleTap() {
-    print('Double tapped!');
-  }
-
-  void handleLongPress() {
-    print('Long pressed!');
-  }
+  void _handleLinkTap() => debugPrint('Link clicked!');
+  void _handleDoubleTap() => debugPrint('Double tapped!');
+  void _handleLongPress() => debugPrint('Long pressed!');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.all(24.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -41,7 +51,7 @@ class LoginPage extends StatelessWidget {
               ),
               const SizedBox(height: 10),
               const Text(
-                'Welcome back! \nYou\'ve been missed!',
+                'Welcome back!\nYou\'ve been missed!',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontWeight: FontWeight.w500,
@@ -54,23 +64,46 @@ class LoginPage extends StatelessWidget {
                 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
                 height: 200,
               ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: loginUser,
-                child: const Text(
-                  'Click me!',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+              const SizedBox(height: 20),
+              TextField(
+                controller: userNameController,
+                decoration: const InputDecoration(
+                  hintText: 'Add your username',
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 12),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Type your password',
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+                  border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: loginUser,
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
               InkWell(
                 splashColor: Colors.red,
-                onTap: handleUrlTap,
-                onDoubleTap: handleDoubleTap,
-                onLongPress: handleLongPress,
+                onTap: _handleLinkTap,
+                onDoubleTap: _handleDoubleTap,
+                onLongPress: _handleLongPress,
                 child: Column(
                   children: const [
                     Text('Find us on'),
+                    SizedBox(height: 4),
                     Text(
                       'https://google.com',
                       style: TextStyle(
