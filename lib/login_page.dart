95717f385c56d45220bf59ccfe7f8ddb0119639d
Palmares-jack <palmaresjack81@gmail.com>
2025-05-30 01:06:37 +0800

    Taps and gesture detection

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,63 +3,80 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  void loginUser() {
+    print('login! successful!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      drawer: const Drawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Button clicked');
-        },
-        child: const Icon(Icons.login),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Let\'s sign you in!',
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.brown,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 0.5,
-              ),
-            ),
-            const SizedBox(height: 8),
-            const Text(
-              'Welcome back!\nYou\'ve been missed!',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.blueGrey,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              height: 200,
-            ),
-            const SizedBox(height: 20),
-            Container(
-              height: 150,
-              width: 150,
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                  fit: BoxFit.fitHeight,
-                  image: NetworkImage(
-                    'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                  ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Let\'s sign you in!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.5,
                 ),
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(24),
               ),
-            ),
-          ],
+              const SizedBox(height: 8),
+              const Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20,
+                  color: Colors.blueGrey,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Image.network(
+                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                height: 200,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: loginUser,
+                child: const Text(
+                  'Click me!',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+                ),
+              ),
+              const SizedBox(height: 10),
+              InkWell(
+                splashColor: Colors.red,
+                onDoubleTap: () {
+                  print('double tapped!');
+                },
+                onLongPress: () {
+                  print('onLongPress!');
+                },
+                onTap: () {
+                  print('Link clicked!');
+                },
+                child: Column(
+                  children: const [
+                    Text('Find us on'),
+                    Text(
+                      'https://google.com',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
