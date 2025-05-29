43ee816edf070a835732f8904cff144e3cb3be15
Palmares-jack <palmaresjack81@gmail.com>
2025-05-30 02:51:41 +0800

    Generics in Dart

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,41 +1,71 @@
+import 'package:flutter/material.dart';
 import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
-import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  ChatPage({Key? key}) : super(key: key);
+
+  final List<ChatMessageEntity> _messages = [
+    ChatMessageEntity(
+      author: Author(userName: 'pooja'),
+      createdAt: 2131231242,
+      id: '1',
+      text: 'First text',
+    ),
+    ChatMessageEntity(
+      author: Author(userName: 'pooja'),
+      createdAt: 2131231442,
+      id: '2',
+      text: 'Second text',
+      imageUrl: 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+    ),
+    ChatMessageEntity(
+      author: Author(userName: 'jane'),
+      createdAt: 2131234242,
+      id: '3',
+      text: 'Third text',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     final username = ModalRoute.of(context)!.settings.arguments as String;
 
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: Center(child: Text('Hi $username!')),
-    actions: [
-    IconButton(
-    onPressed: () {
-    Navigator.pushReplacementNamed(context, '/');
-    print('Logout icon pressed!');
-    },
-    icon: Icon(Icons.logout),
-    ),
-    ],
-    ),
-    body: Column(
-    children: [
-    Expanded(
-    child: ListView.builder(
-    itemCount: 10,
-    itemBuilder: (context, index) {
-    return ChatBubble(
-    entity: ChatMessageEntity(
-    id: 'msg_$index',
-    text: index % 2 == 0
-    ? "Hello, this is Ivan!"
-        : "Hello, this is Mark!!!",
-    createdAt: DateTime.now().millisecondsSinceEpoch,
-    a
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, '/');
+              print('Logout pressed!');
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return ChatBubble(
+                  alignment: message.author.userName == 'pooja'
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
+                  entity: message,
+                );
+              },
+            ),
+          ),
+          ChatInput(),
+        ],
+      ),
+    );
+  }
+}
--- lib/counter_stateful_demo.dart
+++ lib/counter_stateful_demo.dart
@@ -1,63 +1,40 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class CounterStateful extends StatefulWidget {
+  final Color buttonColor;
 
-  void loginUser() {
-    print('Login successful!');
+  const CounterStateful({Key? key, required this.buttonColor})
+      : super(key: key);
+
+  @override
+  State<CounterStateful> createState() => _CounterStatefulState();
+}
+
+class _CounterStatefulState extends State<CounterStateful> {
+  int counter = 0;
+
+  void increment() {
+    if (mounted) {
+      setState(() {
+        counter++;
+      });
+    }
+    print(counter);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: widget.buttonColor,
+        child: const Icon(Icons.add),
+        onPressed: increment,
+      ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'Let\'s sign you in!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Welcome back! \nYou\'ve been missed!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                  color: Colors.blueGrey,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Image.network(
-                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                height: 200,
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: loginUser,
-                child: const Text(
-                  'Click me!',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  print('Pressed on the URL!');
-                },
-                child: const Text('https://google.com'),
-              ),
-            ],
-          ),
+        child: Text(
+          '$counter',
+          style: const TextStyle(fontSize: 30),
         ),
       ),
     );
