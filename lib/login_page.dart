5eb136ef1491b3309edf486e21b293c3e6b4f998
Palmares-jack <palmaresjack81@gmail.com>
2025-05-30 01:09:07 +0800

    Learning about buttons

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
 
+// CounterStateful Widget
+class CounterStateful extends StatefulWidget {
+  final Color buttonColor;
+
+  const CounterStateful({Key? key, required this.buttonColor}) : super(key: key);
+
+  @override
+  State<CounterStateful> createState() => _CounterStatefulState();
+}
+
+class _CounterStatefulState extends State<CounterStateful> {
+  int counter = 0;
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+    print(counter);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Add any initialization code here if needed
+  }
+
+  @override
+  void dispose() {
+    // Add any cleanup code here if needed
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: widget.buttonColor,
+        onPressed: increment,
+        child: const Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text(
+          '$counter',
+          style: const TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
+
+// LoginPage Widget
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   void loginUser() {
-    print('login! successful!');
+    print('Login successful!');
   }
 
   @override
@@ -15,21 +69,21 @@ class LoginPage extends StatelessWidget {
           padding: const EdgeInsets.all(24.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Text(
                 'Let\'s sign you in!',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 30,
-                  color: Colors.black,
+                  color: Colors.brown,
                   fontWeight: FontWeight.bold,
                   letterSpacing: 0.5,
                 ),
               ),
-              const SizedBox(height: 8),
+              const SizedBox(height: 10),
               const Text(
-                'Welcome back! \n You\'ve been missed!',
+                'Welcome back! \nYou\'ve been missed!',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontWeight: FontWeight.w500,
@@ -42,7 +96,7 @@ class LoginPage extends StatelessWidget {
                 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
                 height: 200,
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 30),
               ElevatedButton(
                 onPressed: loginUser,
                 child: const Text(
@@ -50,30 +104,11 @@ class LoginPage extends StatelessWidget {
                   style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
                 ),
               ),
-              const SizedBox(height: 10),
-              InkWell(
-                splashColor: Colors.red,
-                onDoubleTap: () {
-                  print('double tapped!');
+              TextButton(
+                onPressed: () {
+                  print('Pressed on the URL!');
                 },
-                onLongPress: () {
-                  print('onLongPress!');
-                },
-                onTap: () {
-                  print('Link clicked!');
-                },
-                child: Column(
-                  children: const [
-                    Text('Find us on'),
-                    Text(
-                      'https://google.com',
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ],
-                ),
+                child: const Text('https://google.com'),
               ),
             ],
           ),
